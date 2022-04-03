6cbdfffefad59e9afb7cc8cc3c117f2d239f546e
Manu <mcabral@frba.utn.edu.ar>
2022-04-02 21:42:53 -0300

    #2 Added footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer() {
+  return (
+    <footer className="footer">
+        <div className="footer-container">
+            <div className="footer-text">
+                Copyright © {new Date().getFullYear()} Parches
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
