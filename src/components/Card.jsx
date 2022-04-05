38804f0d3b58733100bef51409bad64f58f5e433
Manu <mcabral@frba.utn.edu.ar>
2022-04-05 11:36:49 -0300

    #4 added card base and style

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,20 @@
-function Card({title, paragraph}) {
+function Card({title, paragraph, backgroundImage, backgroundColor}) {
+    const opacity = !backgroundImage? 1 : 0.5;
     return (
         <div className="card">
-            <h3 className="card__title">{title}</h3>
-            <p className="card__paragraph">{paragraph}</p>
+            {
+                backgroundImage &&
+                <div className="card-background">
+                    <img className="card__image" src={backgroundImage} alt="background" />
+                </div>
+            }
+            <div className="card-filter" style={{backgroundColor, opacity}}/> 
+            <div className="card-content">
+                <h3 className="card__title">{title}</h3>
+                <p className="card__paragraph">{paragraph}</p>
+            </div>
         </div>
     )
 }
-  
+
 export default Card
--- src/pages/home/sections/Activities.jsx
+++ src/pages/home/sections/Activities.jsx
@@ -1,14 +1,29 @@
 import Button from "../../../components/Button";
 import Card from "../../../components/Card";
 import image1 from "../../../assets/Activities/image1.png"
+import bodyshaping from "../../../assets/Activities/bodyshaping.jpg"
 
 export default function Activities() {
     return (
         <div className="activities">
             <div className="cards">
-                <Card paragraph="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure consequatur nulla ab accusantium ea eos, blanditiis cum, enim amet soluta doloribus rem? Nulla odio corrupti a illo non, nemo accusantium?" />
-                <Card paragraph="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure consequatur nulla ab accusantium ea eos, blanditiis cum, enim amet soluta doloribus rem? Nulla odio corrupti a illo non, nemo accusantium?" />
-                <Card paragraph="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure consequatur nulla ab accusantium ea eos, blanditiis cum, enim amet soluta doloribus rem? Nulla odio corrupti a illo non, nemo accusantium?" />
+                <Card
+                    title={"Body shaping"}
+                    backgroundImage={bodyshaping}
+                    backgroundColor={"#f5f5f5"}
+                    paragraph="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure consequatur nulla ab accusantium ea eos, blanditiis cum, enim amet soluta doloribus rem? Nulla odio corrupti a illo non, nemo accusantium?"
+                />
+                <Card
+                    title={"Extreme fitness"}
+                    backgroundImage={bodyshaping}
+                    backgroundColor={"#939393"}
+                    paragraph="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure consequatur nulla ab accusantium ea eos, blanditiis cum, enim amet soluta doloribus rem? Nulla odio corrupti a illo non, nemo accusantium?"
+                />
+                <Card
+                    title={"Yoga classis"}
+                    backgroundColor={"#181818"}
+                    paragraph="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure consequatur nulla ab accusantium ea eos, blanditiis cum, enim amet soluta doloribus rem? Nulla odio corrupti a illo non, nemo accusantium?"
+                />
             </div>
             <Button text="Conocer más" />
             <img className="activities__image" src={image1} alt="girlActivity" />
