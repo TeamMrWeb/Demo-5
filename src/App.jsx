a18149935fea5b56e4f39a14ab7ebdf5c960dbe5
Manu <mcabral@frba.utn.edu.ar>
2022-04-04 21:06:55 -0300

    #16 Added about page template

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import {Route, Routes} from "react-router-dom";
 import Home from "./pages/home/Home";
 import Footer from "./components/Footer"
 import NotFound from "./pages/NotFound";
+import About from "./pages/About";
+
 export default function App() {
     return (
         <div className="app">
             <Routes>
                 <Route exact path="/" element={<Home />} />
+                <Route exact path="/nosotros" element={<About />}/>
                 <Route path="*" element={<NotFound />} />
             </Routes>
             <Footer />
---
+++ src/pages/About.jsx
@@ -0,0 +1,22 @@
+export default function About() {
+  return (
+    <section className="about-section">
+        <div className="about-container">
+            <div className="about-header">
+                <h1 className="about__title">
+                    Nuestros entrenadores profesionales
+                </h1>
+                <h2 className="about__subtitle">
+                    En gimnasio tachanka contamos con los mejores profesores para que alcances tu cuerpo soñado
+                </h2>
+                <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias consequuntur velit, perspiciatis culpa explicabo nobis perferendis voluptatibus a sapiente dignissimos modi magni, atque, obcaecati beatae iure eaque cumque commodi illum.
+                </p>
+            </div>
+            <div className="about-content">
+                {/* Cards */}
+            </div>
+        </div>
+    </section>
+  )
+}
