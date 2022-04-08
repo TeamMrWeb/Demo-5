6aa0e15841418bc0153e03384bae6fd990cc4e70
Manu <mcabral@frba.utn.edu.ar>
2022-04-08 19:46:46 -0300

    About page structure finished

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,7 @@
 import mainFigure from "../assets/figure.png"
 import Card from "../components/Card"
+import Title from "../components/Title"
+
 export default function About() {
   return (
     <section className="about-section">
@@ -9,28 +11,36 @@ export default function About() {
                     <img className="about__image" src={mainFigure} alt="mainFigure" />
                 </div>
                 <div className="about-right">
-                    <h1 className="about__title"> Nuestro entrenadores <span className="title-color">profesionales</span> </h1>
-                    <h2 className="about__subtitle"> En <span className="title-color">gimnasio</span> <span className="text-white">Tachanka</span> contamos con los mejores <span className="title-color">profesores</span> para que alcances tu cuerpo <span className="text-white">soñado</span> </h2>
+                    <Title 
+                        title="Nuestro entrenadores" 
+                        titleColor=" profesionales" 
+                        subtitle={"En gimnasio Tachanka contamos con los mejores profesores para que alcances tu cuerpo"} 
+                        subtitleColor={" soñado"}
+                    />
                     <p className="about__description"> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias consequuntur velit, perspiciatis culpa explicabo nobis perferendis voluptatibus a sapiente dignissimos modi magni, atque, obcaecati beatae iure eaque cumque commodi illum.</p>
                 </div>
             </div>
             <div className="about-content">
-                <h1 className="about__title"> Nuestro <span className="title-color">entrenadores</span> </h1>
+                    <Title 
+                        title="Nuestro" 
+                        titleColor=" entrenadores" 
+                    />
                 <div className="about-cards">
                     <Card
                         title={"Juan Perez"}
                         backgroundImage={mainFigure}
-                        backgroundColor={"#f5f5f5"}
+                        backgroundColor={"#181818"}
                         paragraph="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure consequatur nulla ab accusantium ea eos, blanditiis cum, enim amet soluta doloribus rem? Nulla odio corrupti a illo non, nemo accusantium?"
                     />
                     <Card
                         title={"Juan Quispe"}
                         backgroundImage={mainFigure}
-                        backgroundColor={"#939393"}
+                        backgroundColor={"#181818"}
                         paragraph="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure consequatur nulla ab accusantium ea eos, blanditiis cum, enim amet soluta doloribus rem? Nulla odio corrupti a illo non, nemo accusantium?"
                     />
                     <Card
                         title={"Juan Borgoño"}
+                        backgroundImage={mainFigure}
                         backgroundColor={"#181818"}
                         paragraph="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure consequatur nulla ab accusantium ea eos, blanditiis cum, enim amet soluta doloribus rem? Nulla odio corrupti a illo non, nemo accusantium?"
                     />
