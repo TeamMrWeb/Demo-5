3d01ea71321c264c95c8151bef39643bfd699260
Manu <mcabral@frba.utn.edu.ar>
2022-05-15 02:09:10 -0300

    added domain name in packagejs

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,28 @@
-import { Route, Routes } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Schedule from "./pages/Schedule";
-import NotFound from "./pages/NotFound";
-import Classes from "./pages/Classes";
-import Contact from "./pages/Contact";
-import Home from "./pages/home/Home";
-import About from "./pages/About";
-import Class from "./pages/Class";
+import { Route, Routes } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Schedule from './pages/Schedule'
+import NotFound from './pages/NotFound'
+import Classes from './pages/Classes'
+import Contact from './pages/Contact'
+import Home from './pages/home/Home'
+import About from './pages/About'
+import Class from './pages/Class'
 
 export default function App() {
-  return (
-    <main className="app">
-      <Navbar />
-      <Routes>
-        <Route exact path={`/`} element={<Home />} />
-        <Route exact path={`/actividades`} element={<Classes />} />
-        <Route exact path={`/actividad`} element={<Class />} />
-        <Route exact path={`/calendario`} element={<Schedule />} />
-        <Route exact path={`/nosotros`} element={<About />} />
-        <Route exact path={`/contacto`} element={<Contact />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-      <Footer />
-    </main>
-  );
+	return (
+		<main className='app'>
+			<Navbar />
+			<Routes>
+				<Route path={`/`} element={<Home />} />
+				<Route exact path={`/actividades`} element={<Classes />} />
+				<Route exact path={`/actividad`} element={<Class />} />
+				<Route exact path={`/calendario`} element={<Schedule />} />
+				<Route exact path={`/nosotros`} element={<About />} />
+				<Route exact path={`/contacto`} element={<Contact />} />
+				<Route path='*' element={<NotFound />} />
+			</Routes>
+			<Footer />
+		</main>
+	)
 }
